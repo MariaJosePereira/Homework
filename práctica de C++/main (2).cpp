af58b1e590026f16022984103e98240fa48a4ab5
María José Pereira <98987068+MariaJosePereira@users.noreply.github.com>
2022-02-26 15:45:07 -0400

    Add files via upload

---
+++ práctica de C++/main (2).cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1;
+    int number2;
+    
+    cout << "Enter the first number : ";
+    cin >> number1;
+    
+    cout << "Enter the second number : ";
+    cin >> number2;
+    
+    cout << "Score : " << (number1 + number2) << endl;
+    
+    return 0;
+}
+    
---
+++ práctica de C++/main.cpp
@@ -0,0 +1,62 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int number1;
+    int number2;
+    
+    cout << "Enter the first number : ";
+    cin >> number1;
+    
+    cout << "Enter the second number : ";
+    cin >> number2;
+    
+    cout << "Score : " << (number1 + number2) << endl;
+    
+    int number3;
+    int number4;
+    
+    cout << "Enter the third number : ";
+    cin >> number3;
+    
+    cout << "Enter the fourth number : ";
+    cin >> number4;
+    
+    cout << "Score : " << (number3 - number4) << endl;
+    
+    int number5;
+    int number6;
+    
+    cout << "Enter the fifth number : ";
+    cin >> number5;
+    
+    cout << "Enter the sixth number : ";
+    cin >> number6;
+    
+    cout << "Score : " << (number5 / number6) << endl;
+    
+    int number7;
+    int number8;
+    
+    cout << "Enter the seventh number : ";
+    cin >> number7;
+    
+    cout << "Enter the eighth number : ";
+    cin >> number8;
+    
+    cout << "Score : " << (number7 * number8) << endl;
+    
+
+    return 0;
+}
